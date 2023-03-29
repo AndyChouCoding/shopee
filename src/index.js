0a05f4e8436caaaa4ea599955c054b3b0af4aac5
AndyChouCoding <andychouforwk@gmail.com>
2023-03-29 21:44:24 +0800

    3/29
    test

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
 import "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css"
 import "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js"
+import ""
 
+function collapseMobileHandler () {
+    const collapseMobile = document.getElementById("filter-mobile-collapse");
+    const collapseBtnMobile = document.getElementById("filter-mobile-collapse-btn");
+    const collapseBtnTextMobile =document.getElementById("filter-mobile-btn-text");
+    const filterIconMobile = document.getElementById("filter-mobile-icon")
+    const bsCollapseMobile = new Collapse(collapseMobile,{
+        toggle:false,
+    });
+
+
+    collapseBtnMobile.addEventListener("click",function(){
+        bsCollapseMobile.toggle();
+    });
+    collapseMobile.addEventListener("show.bs.collapse",function(){
+        collapseBtnTextMobile.innerText("收起");
+    })
+
+}
 
